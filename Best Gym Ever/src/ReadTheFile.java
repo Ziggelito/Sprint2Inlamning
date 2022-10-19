b752c21f68dcc402746da360ba8eca7c8bbaaae8
Ziggelito <Sixten.Lonnberg@yh.nackademin.se>
2022-10-19 22:01:37 +0200

    nåt

--- Best Gym Ever/src/ReadTheFile.java
+++ Best Gym Ever/src/ReadTheFile.java
@@ -1,21 +1,32 @@
+import jdk.internal.module.ModuleInfoExtender;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ReadTheFile {
-    public ReadTheFile() {
-        String temp;
-        try (BufferedReader BuR = new BufferedReader(new FileReader("src/Memberlist.txt"))) {
-            while((temp = BuR.readLine()) !=null){
-                System.out.println(temp);
+    public static String ReadTheFile() {
+        Path list = Paths.get("Best Gym Ever/src/Memberlist.txt");
+        Path list2 = Paths.get("Best Gym Ever/src/Checkins.txt");
 
+        String temp = null;
+        try (BufferedReader BuR = Files.newBufferedReader(list);
+        BufferedWriter BuW = Files.newBufferedWriter(list2)) {
+            while ((temp = BuR.readLine()) != null) {
+
+                BuW.write(temp);
+                BuW.newLine();
             }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
         }
+        return temp;
 
     }
 }
